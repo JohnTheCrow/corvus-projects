0670bc9290c24fba4b6abfe2830a79896698bb25
John Siddle <john.w.siddle@gmail.com>
2014-06-03 23:59:00 -0400

    fancier epoch convert

--- python/epochconvert.py
+++ python/epochconvert.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 from datetime import *
+import sys
 
-x = raw_input("Enter an epoch timestamp: ")
-print datetime.fromtimestamp(float(x))
+if len(sys.argv) == 1:
+	x = raw_input("Enter an epoch timestamp: ")
+	print datetime.fromtimestamp(float(x))
+elif len(sys.argv) == 2:
+	x = sys.argv[1]
+	print datetime.fromtimestamp(float(x))
+else:
+	print "Enter just one arguement."
