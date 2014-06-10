0f5368f2a9f0a8be82e9774f3cb972522968c30d
John Siddle <john.w.siddle@gmail.com>
2014-06-09 22:17:36 -0400

    better byteconvert

--- python/byteconvert.py
+++ python/byteconvert.py
@@ -1,48 +1,65 @@
 #!/usr/bin/env python
 
-# It would be cool to rewrite this jam to take command line arguments. Maybe
-# rework the conversion math as a function (functions?).
+import sys
 
-size = ["b", "K", "M", "G"]
+usage = "'byteconvert [SIZE]' where SIZE is a number followed by b, k, m, g, or t. E.g. '1024m'."
 
-print "Enter a number followed immediately by a file size.\n\
-Allowed file sizes are b, K, M, and G. For example: '12M'.\n\
-Note that binary prefixes are assumed, i.e. 1K = 1024b.\n\
-Stop with Ctrl+C, Ctrl+D, or by pulling your computer's power cord."
+def bconv(value): #Convert bytes to KiB, MiB, GiB, TiB
+	print "%.2f B : %.2f KiB : %.2f MiB : %.2f GiB : %.2f TiB" % \
+	(value, value / 1024, value / (1024 ** 2), value / (1024 ** 3), value / (1024 ** 4))
 
-while 2 > 1:
+def kconv(value): #Convert KiB to bytes, MiB, GiB, TiB
+	print "%.2f B : %.2f KiB : %.2f MiB : %.2f GiB : %.2f TiB" % \
+	(value * 1024, value, value / 1024, value / (1024 ** 2), value / (1024 ** 3))
 
-	num = raw_input("File size> ")
+def mconv(value): #Convert MiB to bytes, KiB, GiB, TiB
+	print "%.2f B : %.2f KiB : %.2f MiB : %.2f GiB : %.2f TiB" % \
+	(value * (1024 ** 2), value * 1024, value, value / 1024, value / (1024 **2))
 
-	if num[-1] not in size:
-		print "Enter a file size that ends in b, K, M, or G."
-	elif num[-1] == "b":
-# Convert b to K, M, and G
-		print \
-num + ' : ' + \
-str(float(num[0:-1]) / 1024) + "K" + ' : ' + \
-str(float(num[0:-1]) / (1024 ** 2)) + "M" + ' : ' + \
-str(float(num[0:-1]) / (1024 ** 3)) + "G"
-	elif num[-1] == "K":
-# Convert K to b, M, and G
-		print \
-str(float(num[0:-1]) * 1024) + 'b'  + ' : ' + \
-num + ' : ' + \
-str(float(num[0:-1]) / 1024) + "M" + ' : ' + \
-str(float(num[0:-1]) / (1024 ** 2)) + "G"
-	elif num[-1] == "M":
-# Convert M to b, K, and G
-		print \
-str(float(num[0:-1]) * (1024 ** 2)) + 'b'  + ' : ' + \
-str(float(num[0:-1]) * 1024) + "K"  + ' : ' + \
-num + ' : ' + \
-str(float(num[0:-1]) / 1024) + "G"
-	elif num[-1] == "G":
-# Convert G to b, K, and M
-		print \
-str(float(num[0:-1]) * (1024 ** 3)) + 'b'  + ' : ' + \
-str(float(num[0:-1]) * (1024 ** 2)) + "K"  + ' : ' + \
-str(float(num[0:-1]) * 1024) + "M" + ' : ' + \
-num
+def gconv(value): #Convert GiB to bytes, KiB, MiB, TiB
+	print "%.2f B : %.2f KiB : %.2f MiB : %.2f GiB : %.2f TiB" % \
+	(value * (1024 ** 3), value * (1024 ** 2), value * 1024, value, value / 1024)
+
+def tconv(value): #Convert TiB to bytes, KiB, MiB, GiB
+	print "%.2f B : %.2f KiB : %.2f MiB : %.2f GiB : %.2f TiB" % \
+	(value * (1024 ** 4), value * (1024 ** 3), value * (1024 ** 2), value * 1024, value)
+
+def determine_unit():
+	if unit == "b":
+		bconv(value)
+	elif unit == "k":
+		kconv(value)
+	elif unit == "m":
+		mconv(value)
+	elif unit == "g":
+		gconv(value)
+	elif unit == "t":
+		tconv(value)
 	else:
-		print "This is a place holder. Don't look it in the eyes."
+		print usage
+
+def is_number(s): #Tests if a string is a number
+	try:
+		float(s)
+		return True
+	except ValueError:
+		return False
+
+if len(sys.argv) == 1: #Only the command was entered
+	data = raw_input("File size: ")
+	if is_number(data[0:-1]) == True:
+		value = float(data[0:-1])
+		unit = data[-1].lower() #Make sure unit is lowercase
+		determine_unit()
+	else:
+		print usage
+elif len(sys.argv) == 2: #One argument was given
+	data = sys.argv[1]
+	if is_number(data[0:-1]) == True:
+		value = float(data[0:-1])
+		unit = data[-1].lower()
+		determine_unit()
+	else:
+		print usage
+else:
+	print usage
