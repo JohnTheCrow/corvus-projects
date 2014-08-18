b27ef8975822861c78d73080f3d0c61fcb2ce528
John Siddle <john.w.siddle@gmail.com>
2014-08-17 21:36:18 -0400

    study c/unitconvert.c

--- c/stringreverse.c
+++ c/stringreverse.c
@@ -5,7 +5,8 @@ main(){
 	char s[LIMIT];
 	int i, c;
 
-	for(i = 0; i < LIMIT && (c = getchar()) != EOF && c != '\n'; ++i){
+	 /*28-06-14 changed LIMIT to LIMIT-1 */
+	for(i = 0; i < LIMIT-1 && (c = getchar()) != EOF && c != '\n'; ++i){
 		s[i] = c;
 	}
 	if(c == '\n'){
