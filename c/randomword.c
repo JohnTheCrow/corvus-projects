34c89fac8f31a3d4ac90d742a23aa14d62e95c4b
John Siddle <john.w.siddle@gmail.com>
2014-10-28 18:42:26 -0400

    error checking

--- c/randomword.c
+++ c/randomword.c
@@ -7,12 +7,19 @@ int main(void)
 {
 
 /* 4953675 is the number of chars in /usr/share/dict/linux.words minus a few.
- * This is necessary as trying to read past the file results in funkiness. */
+ * The subtraction is necessary as trying to read past the file results in
+ * funkiness. */
 
 	srand(time(NULL));
 	unsigned long seed = rand() % 4953675;
 
 	FILE *fp = fopen("/usr/share/dict/linux.words", "r");
+	if(fp == 0)
+	{
+		printf("Unable to find file.\n");
+		exit(1);
+	}
+
 	fseek(fp, seed, SEEK_SET);
 
 	while(fgetc(fp) != '\n') 
