90c7ee40e1194cac8cb13b756784d2aae287b6b4
John Siddle <john.w.siddle@gmail.com>
2014-10-28 17:44:43 -0400

    randomword.c

---
+++ c/randomword.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* Pull a random line out of /usr/share/dict/linux.words and print it to the
+ * terminal. */
+
+int main(void)
+{
+
+/* 4953675 is the number of chars in /usr/share/dict/linux.words minus a few.
+ * This is necessary as trying to read past the file results in funkiness. */
+
+	srand(time(NULL));
+	unsigned long seed = rand() % 4953675;
+
+	FILE *fp = fopen("/usr/share/dict/linux.words", "r");
+	fseek(fp, seed, SEEK_SET);
+
+	while(fgetc(fp) != '\n') 
+	{
+		;
+	} //File pointer is now at beginning of a word
+
+	char word[80];
+	fgets(word, 80, fp);
+
+	printf("%s", word);
+
+	fclose(fp);
+}
