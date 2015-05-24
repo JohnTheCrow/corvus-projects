00f589a68aa3930ef99647e0e25a85651ce5cc6a
John Siddle <john.w.siddle@gmail.com>
2015-05-24 11:30:10 -0400

    files must have equal number of lines now

--- c/recombobulate.c
+++ c/recombobulate.c
@@ -2,30 +2,52 @@
 #include <string.h>
 
 /* Take line 1 in files 1 and 2 and print them on one line. Do same for all
- * lines of file. Currently uses a space for the delimiter. No checking is
- * performed to see if files have the same number of lines and behavior
- * unpredictable if this is the case. */
+ * lines of file. Files must have an equal number of lines. Currently uses a
+ * space for the delimiter. */
 
 //TODO: Allow for more than two files to be recombobulated
 //TODO: Allow for choice of delimiter between same-line strings
 
 int main(int argc, char **argv)
 {
-
 	FILE *file1 = fopen(argv[1], "r");
 	FILE *file2 = fopen(argv[2], "r");
-	char buffer1[80], buffer2[80];
+	char buffer1[80], buffer2[80], ch;
+	int nr_lines1 = 0; int nr_lines2 = 0;
 
 	if(argc != 3)
 	{
 		printf("Usage: recombobulate file1 file2\n");
 		return 1;
 	}
+
 	if(file1 == 0 || file2 == 0)
 	{
 		printf("One or both of the files could not be found.\n");
 		return 1;
 	}
+
+	while(!feof(file1))
+	{
+		ch = fgetc(file1);
+		if(ch == '\n')
+			nr_lines1++;
+	}
+	while(!feof(file2))
+	{
+		ch = fgetc(file2);
+		if(ch == '\n')
+			nr_lines2++;
+	}
+
+	if(nr_lines1 != nr_lines2)
+	{
+		printf("The files do not have an equal number of lines.\n");
+		return 1;
+	}
+
+	fseek(file1, 0, SEEK_SET);
+	fseek(file2, 0, SEEK_SET);
 
 	while(fgetc(file1) != EOF)
 	{
