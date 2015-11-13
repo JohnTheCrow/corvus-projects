3500b375e3089960aff40eeb95d5a466bd44dee9
John Siddle <john.w.siddle@gmail.com>
2015-11-13 11:24:54 -0500

    added option to delete .rpm file

--- bash/rpmunpacker.sh
+++ bash/rpmunpacker.sh
@@ -8,6 +8,10 @@ if [ -f $1 ]; then
 	PKGDIR=$(rpm -qp --qf '%{NAME}' $1 )
 	mkdir $PKGDIR
 	cp $1 $PKGDIR
+	read -ep 'Do you want to delete the original .rpm file? (y/n) ' ANS
+	if [ $ANS == "y" ]; then
+		rm $1
+	fi
 	cd $PKGDIR
 	rpm2cpio $1 | cpio -idm --quiet
 	rm -f $1
