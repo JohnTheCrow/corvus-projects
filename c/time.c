e1e077f2de0c6521c9a5e7e1125f4b835d22fa98
John Siddle <john.w.siddle@gmail.com>
2015-12-14 21:17:21 -0500

    added date displaying stuffs

--- c/time.c
+++ c/time.c
@@ -20,5 +20,16 @@ int main(int argc, char *argv[])
 /* Converts a tm struct to epoch. */
 	time_t t2 = mktime(&t_s);
 
+/* Print the time in a human friendly format. ctime_r() takes a time_t pointer
+ * and asctime_r() takes a struct tm pointer. Both return a newline and null
+ * terminated string that will be 26 bytes. */
+
+	char *buf1 = (char *)malloc(26);
+	char *buf2 = (char *)malloc(26);
+	ctime_r(&t, buf1);
+	asctime_r(&t_s, buf2);
+
+	printf("%s%s", buf1, buf2);
+
 	return 0;
 }
