274b71170463e418d4a8e241339e85e557001e11
John Siddle <john.w.siddle@gmail.com>
2016-03-19 20:54:43 -0400

    fixed for potential non-alpha input

--- python/rovarspraket.py
+++ python/rovarspraket.py
@@ -14,7 +14,7 @@ string_in  = raw_input("Enter a string: ")
 string_out = ""
 
 for letter in string_in:
-    if letter not in vowels:
+    if letter not in vowels and letter.isalpha():
         letter = letter + 'o' + letter
     string_out += "".join(letter)
 
